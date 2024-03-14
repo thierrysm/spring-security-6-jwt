dab456527913d35ec77fbc270958068b6cd9eb3e
Thierry Marinho <thierrydev48@gmail.com>
2024-03-14 19:04:43 -0300

    build: create application

---
+++ src/main/java/com/tsm/springsecurity6jwt/SpringSecurity6JwtApplication.java
@@ -0,0 +1,13 @@
+package com.tsm.springsecurity6jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity6JwtApplication.class, args);
+	}
+
+}
